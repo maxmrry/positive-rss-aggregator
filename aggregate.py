9772d01939b1e89e72619be8226e4adcdeef010f
maxmrry <105876718+maxmrry@users.noreply.github.com>
2026-04-05 18:59:02 +0200

    Update aggregate.py

--- aggregate.py
+++ aggregate.py
@@ -8,10 +8,37 @@ import re
 
 # --- CONFIGURATION ---
 FEEDS = [
-    "http://www.youtube.com/feeds/videos.xml?channel_id=UCp6COGFcWCnEx9JbPIoYJLw",
+    "http://www.youtube.com/feeds/videos.xml?channel_id=UCE6acMV3m35znLcf0JGNn7Q", # gibiasmr
+    "http://www.youtube.com/feeds/videos.xml?channel_id=UCsr9ZL41tukMWLrqK-Edqaw", # kjtingles
+    "http://www.youtube.com/feeds/videos.xml?channel_id=UCVtQes1mJsOQVxqloWiMXxg", # tinglesbyjess
+    "http://www.youtube.com/feeds/videos.xml?channel_id=UCBsVm9XFIPhnyerJiSlCv5g", # caitasmr
+    "http://www.youtube.com/feeds/videos.xml?channel_id=UCoviXqo4b1MAAkjRWEPJrBg", # busybasmr
+    "http://www.youtube.com/feeds/videos.xml?channel_id=UCGEgrbsgWhp2Pd0JTO45A5w", # beezasmr
+    "http://www.youtube.com/feeds/videos.xml?channel_id=UCFmL725KKPx2URVPvH3Gp8w", # asmrglow
+    "http://www.youtube.com/feeds/videos.xml?channel_id=UCJyZfWrqaGX4nwXGKOEdM6Q", # nanouasmr
+    "http://www.youtube.com/feeds/videos.xml?channel_id=UCgUBjMqA_IZQVFdEElvPHDA", # lunabloomasmr
+    "http://www.youtube.com/feeds/videos.xml?channel_id=UC79B6k5KZxYLETtXFfZH1cg", # slightsoundsasmr
+    "http://www.youtube.com/feeds/videos.xml?channel_id=UCxL_v3JKcd3_h5OYralxvPg", # darkliteasmr
+    "http://www.youtube.com/feeds/videos.xml?channel_id=UCr122mXAEYZPcVi8-kCZP8g", # asmrgirl
+    "http://www.youtube.com/feeds/videos.xml?channel_id=UC4bOYyhQgBVzk-neOMHJilQ", # itsbunnii
+    "http://www.youtube.com/feeds/videos.xml?channel_id=UCDwR_uew-XkbO0RjUAgrUug", # sarahlavender
+    "http://www.youtube.com/feeds/videos.xml?channel_id=UCsOK8zTbZ3BQyEuA11i19Wg", # ilamosasmr
+    "http://www.youtube.com/feeds/videos.xml?channel_id=UCnLrvPN5kyK2gCb7pMgCrBw", # baituasmr
+    "http://www.youtube.com/feeds/videos.xml?channel_id=UCW6ulUHXpmumuNZS8rWLXbA", # frecklesasmr
+    "http://www.youtube.com/feeds/videos.xml?channel_id=UCM5z4re0CofPJJTp1Uocb9Q", # safespaceasmr
+    "http://www.youtube.com/feeds/videos.xml?channel_id=UC0beMA4EPX3TfgPB24zH3DA", # paganmoon
+    "http://www.youtube.com/feeds/videos.xml?channel_id=UC2UDe1cKHqOblGo8rHcJ2Vg", # asmrshimmer
+    "http://www.youtube.com/feeds/videos.xml?channel_id=UC9T_5DOidv_3EC9TGRDjZEA", # 11orangesasmr
+    "http://www.youtube.com/feeds/videos.xml?channel_id=UCdYBTauH8tBrY6aJGdieIDA", # hudiasmr
+    "http://www.youtube.com/feeds/videos.xml?channel_id=UCTIxX_suYllEj6n2Q0WVWZQ", # bbyasmr
+    "http://www.youtube.com/feeds/videos.xml?channel_id=UCWwGnRCb6luuHOorUR_JxLw", # cassiabreeasmr
+    "http://www.youtube.com/feeds/videos.xml?channel_id=UCbRuPWKGtsJo4v1LuizilfQ", # asmrleaf
+    "http://www.youtube.com/feeds/videos.xml?channel_id=UCn70vKPtIPkD0W8ISmwlK0A", # kayleescozyasmr
+    "http://www.youtube.com/feeds/videos.xml?channel_id=UCDfpxS7GiqyoUeXS-zAutoA", # emmyasmrr
+    "http://www.youtube.com/feeds/videos.xml?channel_id=UCikebqFWoT3QC9axUbXCPYw", # asmrdarling
 ]
 
-KEYWORDS = ['affirmation', 'affirmations', 'positive']
+KEYWORDS = ['affirmation', 'affirmations', 'subconscious', 'positive']
 
 TIMEZONE = pytz.timezone('Europe/Paris')
 REMINDER_HOUR = 23
@@ -48,7 +75,7 @@ def main():
 
     all_entries = []
 
-    cutoff = datetime.now(TIMEZONE) - timedelta(days=14)
+    cutoff = datetime.now(TIMEZONE) - timedelta(days=60)
 
     # 1. Fetch and Filter YouTube Feeds
     for url in FEEDS:
@@ -83,7 +110,7 @@ def main():
         if now >= reminder_time or i > 0:
             all_entries.append({
                 'title': '✅ Remember to do daily positive affirmations',
-                'link': '',
+                'link': f'https://maxmrry.github.io/positive-rss-aggregator/#reminder-{reminder_time.strftime("%Y%m%d")}',
                 'description': '',
                 'published': reminder_time,
                 'id': f"reminder-{reminder_time.strftime('%Y%m%d')}"
